89651939a5b140a34613a1f3ecdc92c1c2293f9f
dhruv2223 <DHRUV.CHOTALIA017@svkmmumbai.onmicrosoft.com>
2025-03-28 22:26:31 +0530

    db issue solved

--- src/api/database.py
+++ src/api/database.py
@@ -1,11 +1,15 @@
+import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
 
-DB_NAME = "invsto"
-DB_USER = "postgres"
-DB_PASSWORD = "Cdhruv@1234"
-DB_HOST = "localhost"
-DB_PORT = "5432"
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "db")  # Default to 'db' for Docker
+DB_PORT = os.getenv("DB_PORT", "5432")
 
 def get_connection():
     try:
@@ -15,7 +19,7 @@ def get_connection():
             password=DB_PASSWORD,
             host=DB_HOST,
             port=DB_PORT,
-            cursor_factory=RealDictCursor  # Ensures results are dictionaries
+            cursor_factory=RealDictCursor
         )
         return conn
     except Exception as e:
